a427a9f7bf37e0559d6a105cb906403c8090ebdf
119472906 <119472906@umail.ucc.ie>
2021-04-26 16:40:19 +0100

    First commit

---
+++ src/Start_Page/Start_Page.java
@@ -0,0 +1,25 @@
+package Start_Page;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Start_Page extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/Views/Sign_In_Page.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
